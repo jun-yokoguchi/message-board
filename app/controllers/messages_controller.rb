690e20d058c5673c9e6f1c5284f7663893db8c52
jun_yokoguchi <jy.ilcy@gmail.com>
2015-12-20 08:03:27 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     @message = Message.new
     #Messageを全て取得する。
@@ -15,12 +16,35 @@ class MessagesController < ApplicationController
       flash.now[:alert] ="メッセージの保存に失敗しました。"
       render 'index'
   end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      #保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      #保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+  end
+    
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
 end
+
 
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
+  def set_message
+    @message = Message.find(params[:id])
+  end
   ##ここまで
 end
+end
 
+#8.6削除機能の実装でエラー
