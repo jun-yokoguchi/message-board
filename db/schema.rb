58d2cd6e0546707d5da8e0edad3d01a691fcfffe
jun_yokoguchi <jy.ilcy@gmail.com>
2015-12-23 14:47:57 +0000

    challenge2

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20151221210709) do
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
+    t.string   "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "age"
